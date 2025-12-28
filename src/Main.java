84503cf059af06aaac0aa54c3b7dcf66c1a1e18c
anthony <trackun@hotmail.com>
2025-12-28 14:24:28 +0100

    Day 8 - part 1 (WIP)

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
                     new Day4(),
                     new Day5(),
                     new Day6(),
-                    new Day7(), 
+                    new Day7(),
                     new Day8()
             )
     );
--- src/tests/Day8Tests.java
+++ src/tests/Day8Tests.java
@@ -8,11 +8,126 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day8Tests {
+    
+    @Test
+    void testJunctionBoxConstructor() {
+        JunctionBox junctionBox = new JunctionBox("162,817,812");
 
+        Assertions.assertEquals(162, junctionBox.getPosition()[0]);
+        Assertions.assertEquals(817, junctionBox.getPosition()[1]);
+        Assertions.assertEquals(812, junctionBox.getPosition()[2]);
+    }
+
+    @Test
+    void testComputeDistance() {
+        JunctionBox junctionBox = new JunctionBox("162,817,812");
+        JunctionBox junctionBox2 = new JunctionBox("57,618,57");
+        
+        Assertions.assertEquals(787.814, Math.round(junctionBox2.computeDistance(junctionBox)), 1);
+        Assertions.assertEquals(0, Math.round(junctionBox.computeDistance(junctionBox)));
+    }
+
+    @Test
+    void testComputeDistance2() {
+        JunctionBox junctionBox = new JunctionBox("162,817,812");
+        JunctionBox junctionBox2 = new JunctionBox("431,825,988");
+
+        Assertions.assertEquals(321.56, Math.round(junctionBox2.computeDistance(junctionBox)), 1);
+    }
+    
+    @Test
+    void testFindLowestDistance() {
+        JunctionBox junctionBox = new JunctionBox("162,817,812");
+        JunctionBox junctionBox2 = new JunctionBox("57,618,57");
+        JunctionBox junctionBox3 = new JunctionBox("431,825,988");
+        
+        List<JunctionBox> boxes = new ArrayList<>();
+        boxes.add(junctionBox);
+        boxes.add(junctionBox2);
+        boxes.add(junctionBox3);
+        
+        JunctionBox nearest = null;
+        double nearestDistance =  Double.MAX_VALUE;
+        
+        for(int i = 0; i < boxes.size()-1; i++) {
+            for(int j = i+1; j < boxes.size(); j++) {
+                double distance = boxes.get(i).computeDistance(boxes.get(j));
+                if(distance < nearestDistance) {
+                    nearestDistance = distance;
+                    nearest = boxes.get(j);
+                }
+            }
+        }
+        
+        Assertions.assertEquals(321.56, nearestDistance, 1);
+        Assertions.assertNotNull(nearest);
+        Assertions.assertEquals(junctionBox3, nearest);
+    }
+    
+    @Test
+    void testJunctionBoxComputerConstructor() {
+        List<String> list = Arrays.asList("162,817,812", "57,618,57", "431,825,988");
+
+        JunctionBoxComputer junctionBoxComputer = new JunctionBoxComputer(list);
+        
+        Assertions.assertNotNull(junctionBoxComputer);
+        Assertions.assertEquals(3, junctionBoxComputer.getJunctionBoxes().size());
+        Assertions.assertEquals(162, junctionBoxComputer.getJunctionBoxes().getFirst().getPosition()[0]);
+        Assertions.assertEquals(817, junctionBoxComputer.getJunctionBoxes().getFirst().getPosition()[1]);
+        Assertions.assertEquals(812, junctionBoxComputer.getJunctionBoxes().getFirst().getPosition()[2]);
+    }
+    
+    @Test
+    void testFindLowestDistanceWithComputer() {
+        List<String> list = Arrays.asList("162,817,812", "57,618,57", "431,825,988");
+        
+        JunctionBoxComputer junctionBoxComputer = new JunctionBoxComputer(list);
+        
+        Assertions.assertEquals(junctionBoxComputer.getJunctionBoxes().get(2), junctionBoxComputer.getMinimalDistance(0));
+    }
+    
+    @Test
+    void testComputeJunctionBox() {
+        List<String> list = Arrays.asList("162,817,812", "57,618,57", "431,825,988");
+
+        JunctionBoxComputer junctionBoxComputer = new JunctionBoxComputer(list);
+        junctionBoxComputer.computeJunctionBoxes(1);
+        
+        Assertions.assertEquals(1, junctionBoxComputer.getCircuits().size());
+        Assertions.assertEquals(2, junctionBoxComputer.getCircuits().getFirst().size());
+        
+        JunctionBox junctionBox = junctionBoxComputer.getJunctionBoxes().get(0);
+        JunctionBox junctionBox2 = junctionBoxComputer.getJunctionBoxes().get(2);
+        
+        Assertions.assertTrue(junctionBoxComputer.getCircuits().getFirst().contains(junctionBox));
+        Assertions.assertTrue(junctionBoxComputer.getCircuits().getFirst().contains(junctionBox2));
+    }
+
+    @Test
+    void testComputeJunctionBoxWithTwoValues() {
+        List<String> list = Arrays.asList("162,817,812", "57,618,57", "431,825,988");
+
+        JunctionBoxComputer junctionBoxComputer = new JunctionBoxComputer(list);
+        junctionBoxComputer.computeJunctionBoxes(2);
+
+        Assertions.assertEquals(1, junctionBoxComputer.getCircuits().size());
+        Assertions.assertEquals(1, junctionBoxComputer.getNumberOfConnection());
+        Assertions.assertEquals(3, junctionBoxComputer.getCircuits().getFirst().size());
+
+        JunctionBox junctionBox0 = junctionBoxComputer.getJunctionBoxes().get(0);
+        JunctionBox junctionBox1 = junctionBoxComputer.getJunctionBoxes().get(1);
+        JunctionBox junctionBox2 = junctionBoxComputer.getJunctionBoxes().get(2);
+
+        Assertions.assertTrue(junctionBoxComputer.getCircuits().getFirst().contains(junctionBox0));
+        Assertions.assertTrue(junctionBoxComputer.getCircuits().getFirst().contains(junctionBox1));
+        Assertions.assertTrue(junctionBoxComputer.getCircuits().getFirst().contains(junctionBox2));
+    }
     static List<String> list = new ArrayList<>();
+    static List<String> listSlightlyModified = new ArrayList<>();
     
     @BeforeAll
     static void createDataSetForTest() {
@@ -36,6 +151,27 @@ public class Day8Tests {
         list.add("862,61,35");
         list.add("984,92,344");
         list.add("425,690,689");
+        
+        listSlightlyModified.add("57,618,57");
+        listSlightlyModified.add("906,360,560");
+        listSlightlyModified.add("592,479,940");
+        listSlightlyModified.add("162,817,812");
+        listSlightlyModified.add("352,342,300");
+        listSlightlyModified.add("466,668,158");
+        listSlightlyModified.add("542,29,236");
+        listSlightlyModified.add("431,825,988");
+        listSlightlyModified.add("739,650,466");
+        listSlightlyModified.add("52,470,668");
+        listSlightlyModified.add("216,146,977");
+        listSlightlyModified.add("819,987,18");
+        listSlightlyModified.add("117,168,530");
+        listSlightlyModified.add("805,96,715");
+        listSlightlyModified.add("346,949,466");
+        listSlightlyModified.add("970,615,88");
+        listSlightlyModified.add("941,993,340");
+        listSlightlyModified.add("862,61,35");
+        listSlightlyModified.add("984,92,344");
+        listSlightlyModified.add("425,690,689");
     }
     
     @Test
